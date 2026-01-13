079a3ea81352bb5fa711840019e9de4a69a239b3
MiraidaOrozbaeva <orozbaeva_miraida@mail.ru>
2026-01-13 23:25:28 +0600

    added sout in existing class QrPayment added new class Visa Payment

--- src/kg/sim_bank/payments/QrPayment.java
+++ src/kg/sim_bank/payments/QrPayment.java
@@ -3,5 +3,6 @@ package kg.sim_bank.payments;
 public class QrPayment {
     public static void main(String[] args) {
         System.out.println("QR Payment");
+        System.out.println("Hello world!");
     }
 }
---
+++ src/kg/sim_bank/payments/VisaPayment.java
@@ -0,0 +1,7 @@
+package kg.sim_bank.payments;
+
+public class VisaPayment {
+    public static void main(String[] args) {
+        System.out.println("Visa payment");
+    }
+}
