82bb8bae95dd28e9e6d4557ee1a028b427ff2e88
MiraidaOrozbaeva <orozbaeva_miraida@mail.ru>
2026-01-15 09:28:21 +0600

    January promo Miraida
    January promotion sout1
    
    January promotion sout2
    
    January promotion sout3
    
    January promotion sout4
    
    January promotion sout5
    
    January promotion sout6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January promotions");
+        System.out.println("January 123");
+        System.out.println("January 1234");
+        System.out.println("January 12345");
+        System.out.println("January 123456");
+        System.out.println("January 1234567");
+        System.out.println("January 12345678");
+        System.out.println("January 123456789");
+        System.out.println("January 12345678910");
+    }
+}
